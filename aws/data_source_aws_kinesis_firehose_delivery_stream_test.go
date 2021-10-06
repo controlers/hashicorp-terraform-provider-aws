ab84422fbe6d35cfaf69c2add00d420cfb30422d
Dirk Avery <dirk.avery@gmail.com>
2021-10-05 21:59:12 -0400

    firehose: Use acctest

--- aws/data_source_aws_kinesis_firehose_delivery_stream_test.go
+++ aws/data_source_aws_kinesis_firehose_delivery_stream_test.go
@@ -18,7 +18,7 @@ func TestAccDataSourceAwsKinesisFirehoseDeliveryStream_basic(t *testing.T) {
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:     func() { acctest.PreCheck(t) },
 		ErrorCheck:   acctest.ErrorCheck(t, firehose.EndpointsID),
-		Providers:    testAccProviders,
+		Providers:    acctest.Providers,
 		CheckDestroy: testAccCheckKinesisFirehoseDeliveryStreamDestroy,
 		Steps: []resource.TestStep{
 			{
