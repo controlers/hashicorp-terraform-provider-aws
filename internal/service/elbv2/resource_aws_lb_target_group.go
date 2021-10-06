1ea4298796c112b3960b44f5cfb2c6703285f23b
Dirk Avery <dirk.avery@gmail.com>
2021-10-05 23:20:12 -0400

    elbv2: Export/un-export after move

--- internal/service/elbv2/resource_aws_lb_target_group.go
+++ internal/service/elbv2/resource_aws_lb_target_group.go
@@ -23,6 +23,21 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/conns"
 	tftags "github.com/hashicorp/terraform-provider-aws/internal/tags"
 	"github.com/hashicorp/terraform-provider-aws/internal/verify"
+	tfelbv2 "github.com/hashicorp/terraform-provider-aws/internal/service/elbv2"
+	tfelbv2 "github.com/hashicorp/terraform-provider-aws/internal/service/elbv2"
+	tfelbv2 "github.com/hashicorp/terraform-provider-aws/internal/service/elbv2"
+	tfelbv2 "github.com/hashicorp/terraform-provider-aws/internal/service/elbv2"
+	tfelbv2 "github.com/hashicorp/terraform-provider-aws/internal/service/elbv2"
+	tfelbv2 "github.com/hashicorp/terraform-provider-aws/internal/service/elbv2"
+	tfelbv2 "github.com/hashicorp/terraform-provider-aws/internal/service/elbv2"
+	tfelbv2 "github.com/hashicorp/terraform-provider-aws/internal/service/elbv2"
+	tfelbv2 "github.com/hashicorp/terraform-provider-aws/internal/service/elbv2"
+	tfelbv2 "github.com/hashicorp/terraform-provider-aws/internal/service/elbv2"
+	tfelbv2 "github.com/hashicorp/terraform-provider-aws/internal/service/elbv2"
+	tfelbv2 "github.com/hashicorp/terraform-provider-aws/internal/service/elbv2"
+	tfelbv2 "github.com/hashicorp/terraform-provider-aws/internal/service/elbv2"
+	tfelbv2 "github.com/hashicorp/terraform-provider-aws/internal/service/elbv2"
+	tfelbv2 "github.com/hashicorp/terraform-provider-aws/internal/service/elbv2"
 )
 
 func ResourceTargetGroup() *schema.Resource {
@@ -389,10 +404,10 @@ func resourceTargetGroupRead(d *schema.ResourceData, meta interface{}) error {
 
 	var targetGroup *elbv2.TargetGroup
 
-	err := resource.Retry(waiter.PropagationTimeout, func() *resource.RetryError {
+	err := resource.Retry(tfelbv2.propagationTimeout, func() *resource.RetryError {
 		var err error
 
-		targetGroup, err = finder.TargetGroupByARN(conn, d.Id())
+		targetGroup, err = tfelbv2.FindTargetGroupByARN(conn, d.Id())
 
 		if d.IsNewResource() && tfawserr.ErrCodeEquals(err, elbv2.ErrCodeTargetGroupNotFoundException) {
 			return resource.RetryableError(err)
@@ -412,7 +427,7 @@ func resourceTargetGroupRead(d *schema.ResourceData, meta interface{}) error {
 	})
 
 	if tfresource.TimedOut(err) {
-		targetGroup, err = finder.TargetGroupByARN(conn, d.Id())
+		targetGroup, err = tfelbv2.FindTargetGroupByARN(conn, d.Id())
 	}
 
 	if !d.IsNewResource() && tfawserr.ErrCodeEquals(err, elbv2.ErrCodeTargetGroupNotFoundException) {
@@ -444,7 +459,7 @@ func resourceTargetGroupUpdate(d *schema.ResourceData, meta interface{}) error {
 	if d.HasChange("tags_all") {
 		o, n := d.GetChange("tags_all")
 
-		err := resource.Retry(waiter.LoadBalancerTagPropagationTimeout, func() *resource.RetryError {
+		err := resource.Retry(tfelbv2.loadBalancerTagPropagationTimeout, func() *resource.RetryError {
 			err := tftags.Elbv2UpdateTags(conn, d.Id(), o, n)
 
 			if tfawserr.ErrCodeEquals(err, elbv2.ErrCodeTargetGroupNotFoundException) {
@@ -635,7 +650,7 @@ func resourceTargetGroupDelete(d *schema.ResourceData, meta interface{}) error {
 	}
 
 	log.Printf("[DEBUG] Deleting Target Group (%s): %s", d.Id(), input)
-	err := resource.Retry(waiter.TargetGroupDeleteTimeout, func() *resource.RetryError {
+	err := resource.Retry(tfelbv2.targetGroupDeleteTimeout, func() *resource.RetryError {
 		_, err := conn.DeleteTargetGroup(input)
 
 		if tfawserr.ErrMessageContains(err, "ResourceInUse", "is currently in use by a listener or a rule") {
