64948e2b3d89a415829232c0dd632efb883b1f88
Dirk Avery <dirk.avery@gmail.com>
2021-10-06 12:42:38 -0400

    ecrpublic: Making service sweeps

--- internal/service/ecrpublic/repository_test.go
+++ internal/service/ecrpublic/repository_test.go
@@ -19,57 +19,9 @@ import (
 	"github.com/hashicorp/terraform-provider-aws/internal/sweep"
 )
 
-func init() {
-	resource.AddTestSweepers("aws_ecrpublic_repository", &resource.Sweeper{
-		Name: "aws_ecrpublic_repository",
-		F:    sweepRepositories,
-	})
-}
 
-func sweepRepositories(region string) error {
-	client, err := sweep.SharedRegionalSweepClient(region)
 
-	if err != nil {
-		return fmt.Errorf("error getting client: %w", err)
-	}
 
-	conn := client.(*conns.AWSClient).ECRPublicConn
-	sweepResources := make([]*sweep.SweepResource, 0)
-	var errs *multierror.Error
-
-	err = conn.DescribeRepositoriesPages(&ecrpublic.DescribeRepositoriesInput{}, func(page *ecrpublic.DescribeRepositoriesOutput, lastPage bool) bool {
-		if page == nil {
-			return !lastPage
-		}
-
-		for _, repository := range page.Repositories {
-			r := tfecrpublic.ResourceRepository()
-			d := r.Data(nil)
-			d.SetId(aws.StringValue(repository.RepositoryName))
-			d.Set("registry_id", repository.RegistryId)
-			d.Set("force_destroy", true)
-
-			sweepResources = append(sweepResources, sweep.NewSweepResource(r, d, client))
-		}
-
-		return !lastPage
-	})
-
-	if err != nil {
-		errs = multierror.Append(errs, fmt.Errorf("error describing ECR Public Repositories for %s: %w", region, err))
-	}
-
-	if err = sweep.SweepOrchestrator(sweepResources); err != nil {
-		errs = multierror.Append(errs, fmt.Errorf("error sweeping ECR Public Repositories for %s: %w", region, err))
-	}
-
-	if sweep.SkipSweepError(errs.ErrorOrNil()) {
-		log.Printf("[WARN] Skipping ECR Public Repositories sweep for %s: %s", region, errs)
-		return nil
-	}
-
-	return errs.ErrorOrNil()
-}
 
 func TestAccECRPublicRepository_basic(t *testing.T) {
 	var v ecrpublic.Repository
