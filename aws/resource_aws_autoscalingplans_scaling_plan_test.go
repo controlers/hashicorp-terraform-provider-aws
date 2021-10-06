5534318229a6c5ed1be4b0e4a15afb7f16652f3c
Dirk Avery <dirk.avery@gmail.com>
2021-10-05 21:59:02 -0400

    autoscalingplans: Use acctest

--- aws/resource_aws_autoscalingplans_scaling_plan_test.go
+++ aws/resource_aws_autoscalingplans_scaling_plan_test.go
@@ -80,7 +80,7 @@ func TestAccAwsAutoScalingPlansScalingPlan_basicDynamicScaling(t *testing.T) {
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:     func() { acctest.PreCheck(t) },
 		ErrorCheck:   acctest.ErrorCheck(t, autoscalingplans.EndpointsID),
-		Providers:    testAccProviders,
+		Providers:    acctest.Providers,
 		CheckDestroy: testAccCheckAutoScalingPlansScalingPlanDestroy,
 		Steps: []resource.TestStep{
 			{
@@ -132,7 +132,7 @@ func TestAccAwsAutoScalingPlansScalingPlan_basicPredictiveScaling(t *testing.T) 
 			acctest.PreCheckIAMServiceLinkedRole(t, "/aws-service-role/autoscaling-plans")
 		},
 		ErrorCheck:   acctest.ErrorCheck(t, autoscalingplans.EndpointsID),
-		Providers:    testAccProviders,
+		Providers:    acctest.Providers,
 		CheckDestroy: testAccCheckAutoScalingPlansScalingPlanDestroy,
 		Steps: []resource.TestStep{
 			{
@@ -188,7 +188,7 @@ func TestAccAwsAutoScalingPlansScalingPlan_basicUpdate(t *testing.T) {
 			acctest.PreCheckIAMServiceLinkedRole(t, "/aws-service-role/autoscaling-plans")
 		},
 		ErrorCheck:   acctest.ErrorCheck(t, autoscalingplans.EndpointsID),
-		Providers:    testAccProviders,
+		Providers:    acctest.Providers,
 		CheckDestroy: testAccCheckAutoScalingPlansScalingPlanDestroy,
 		Steps: []resource.TestStep{
 			{
@@ -266,14 +266,14 @@ func TestAccAwsAutoScalingPlansScalingPlan_disappears(t *testing.T) {
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:     func() { acctest.PreCheck(t) },
 		ErrorCheck:   acctest.ErrorCheck(t, autoscalingplans.EndpointsID),
-		Providers:    testAccProviders,
+		Providers:    acctest.Providers,
 		CheckDestroy: testAccCheckAutoScalingPlansScalingPlanDestroy,
 		Steps: []resource.TestStep{
 			{
 				Config: testAccAutoScalingPlansScalingPlanConfigBasicDynamicScaling(rName, rName),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckAutoScalingPlansScalingPlanExists(resourceName, &scalingPlan),
-					acctest.CheckResourceDisappears(testAccProvider, resourceAwsAutoScalingPlansScalingPlan(), resourceName),
+					acctest.CheckResourceDisappears(acctest.Provider, resourceAwsAutoScalingPlansScalingPlan(), resourceName),
 				),
 				ExpectNonEmptyPlan: true,
 			},
@@ -282,7 +282,7 @@ func TestAccAwsAutoScalingPlansScalingPlan_disappears(t *testing.T) {
 }
 
 func testAccCheckAutoScalingPlansScalingPlanDestroy(s *terraform.State) error {
-	conn := testAccProvider.Meta().(*AWSClient).autoscalingplansconn
+	conn := acctest.Provider.Meta().(*AWSClient).autoscalingplansconn
 
 	for _, rs := range s.RootModule().Resources {
 		if rs.Type != "aws_autoscalingplans_scaling_plan" {
@@ -309,7 +309,7 @@ func testAccCheckAutoScalingPlansScalingPlanDestroy(s *terraform.State) error {
 
 func testAccCheckAutoScalingPlansScalingPlanExists(name string, v *autoscalingplans.ScalingPlan) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
-		conn := testAccProvider.Meta().(*AWSClient).autoscalingplansconn
+		conn := acctest.Provider.Meta().(*AWSClient).autoscalingplansconn
 
 		rs, ok := s.RootModule().Resources[name]
 		if !ok {
